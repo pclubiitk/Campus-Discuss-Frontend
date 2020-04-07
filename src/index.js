b426aa6ad12828cb04fc7d979d7461e80e55dc0a
NupurJ <nupurj.2001@gmail.com>
2020-04-07 20:19:06 +0530

    Move code from src/index.js to src/components/sidebar/index.js, plus cleaning up code

--- src/index.js
+++ src/index.js
@@ -4,68 +4,7 @@ import './index.css';
 import App from './App';
 import Sidebar from './components/sidebar'
 import * as serviceWorker from './serviceWorker';
-function Profile(props) {
-+    return (
-+        <Button className="contained">{props.name}
-+
-+        </Button>
-+    );
-+}
-+class StreamsList extends React.Component {
-+
-+    render() {
-+        return (
-+            <div class="mdc-menu mdc-menu-surface">
-+                <ul class="mdc-list" role="menu" aria-hidden="false" aria-orientation="horizontal" tabindex="-1">
-+                    <uli class="mdc-list-item" role="menuitem">
-+                        <span class="mdc-list-item__text"></span>
-+
-+
-+
-+                        <Button className="contained" color="primary" onClick={() => alert("clicked " + this.props.streamName)}>
-+                            {this.props.streamName}
-+                        </Button>
-+                                                                     
-+                     </uli>
-+
-+
-+
-+                </ul>
-+            </div>
-+        )
-+    }
-+}
-+class Sidebar extends React.Component {
-+    CreateStreamsList(streams) {
-+        let list = new Array();
-+        console.log(streams);
-+        console.log(streams[1]);
-+        for (let i = 0; i < streams.length; i++) {
-+            list.push(<StreamsList streamName={streams[i]} />);
-+        }
-+        return list;
-+    }
-+    render() {
-+        const streamsList = this.CreateStreamsList(this.props.streams);
-+        return (
-+
-+            <nav>
-+                <div className="sidebar">
-+                    <div className="profile">
-+                        <Profile name={this.props.name} />
-+                    </div>
-+                    <div className="streams-list">
-+                        <ul className="sidenav">
-+                            {streamsList}
-+                        </ul>
-+                    </div>
-+                </div>
-+            </nav>
-+        )
-+    }
-+}
-+
-+
+
 ReactDOM.render(
   <React.StrictMode>
     <Sidebar name="John Doe" streams={["Stream1", "Stream Two"]}/>
