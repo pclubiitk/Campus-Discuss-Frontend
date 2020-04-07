b0f727930c777d3d208594ef546f8bf7fb150ae7
NupurJ <nupurj.2001@gmail.com>
2020-04-07 11:00:07 +0530

    fixed camelCase

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-class Sidebar extends React.component {
+class Sidebar extends React.Component {
     render() {
         return (
-            <div classname="sidebar">
-                <div classname="profile">
+            <div className="sidebar">
+                <div className="profile">
 
                 </div>
-                <div classname="streams-list">
+                <div className="streams-list">
                     
                 </div>
             </div>
