5ef333f01d5277f1fab0f51499893229ab5cae28
NupurJ <nupurj.2001@gmail.com>
2020-04-07 16:00:55 +0530

    Add basic functionality to sidebar - display username and list of streams

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 import './style.css';
 
-class Sidebar extends React.Component {
+function Profile(props) {
+    return (
+        <Button className="contained">{props.name}</Button>
+    );
+}
+
+class StreamsList extends React.Component {
     render() {
         return (
-            <div className="sidebar">
-                <div className="profile">
-
-                </div>
-                <div className="streams-list">
-                    
-                </div>
-            </div>
+            <li>{this.props.streamName}</li>
         )
     }
 }
+class Sidebar extends React.Component {
+    CreateStreamsList(streams) {
+        let list = new Array();
+        console.log(streams);
+        console.log(streams[1]);
+        for (let i = 0; i < streams.length; i++) {
+             list.push(<StreamsList streamName={streams[i]} />);
+        }
+        return list;
+    }
+    render() {
+        const streamsList=this.CreateStreamsList(this.props.streams);
+        return (
+
+            <nav>
+                <div className="sidebar">
+                    <div className="profile">
+                        <Profile name={this.props.name} />
+                    </div>
+                    <div className="streams-list">
+                        <ul className="sidenav">
+                            {streamsList}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <Sidebar />,
+    <Sidebar name="John Doe" streams={["Stream1", "Stream Two"]}/>,
     document.getElementById('root')
 );
