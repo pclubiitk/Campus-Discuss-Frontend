320a31da6fdf19cdbab4ee13cda817ed41ebb173
Devanshu Singla <devanshu803.singla@gmail.com>
2020-04-08 01:43:08 +0530

    change class implementation to functional implementation and removed console.log messages

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,44 +1,37 @@
-import React from 'react' ;
-import './style.css' ;
+import React from "react";
+import "./style.css";
 
-        /*with Material UI*/
+/*using Material UI*/
 
-import { AppBar } from '@material-ui/core' ;
-import { Toolbar } from '@material-ui/core' ;
-import { IconButton } from '@material-ui/core' ;
-import MenuIcon from '@material-ui/icons/Menu' ;
-import Search from '@material-ui/icons/Search' ;
-import { Typography } from '@material-ui/core' ;
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import Search from "@material-ui/icons/Search";
+import { Typography } from "@material-ui/core";
 
-console.log("Opened !")
-
-class Topbar extends React.Component {
-    render() {
-        console.log("in render function")
-        return (
-            <AppBar position="static">
-            <div class="topbar">
-                <div>
-                <Toolbar>
-                    <IconButton onClick={() => console.log("menu clicked")} edge="start" color="inherit" aria-label="menu">
-                    <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" >
-                    { this.props.title }
-                    </Typography>
-                </Toolbar>
-                </div>
-                <div>
-                <Toolbar>
-                    <IconButton onClick={() => console.log("search clicked")} edge="start" color="inherit" aria-label="menu">
-                    <Search />
-                    </IconButton>
-                </Toolbar>
-                </div>
-            </div>
-            </AppBar>
-        )
-    }
+function Topbar(props) {
+  return (
+    <AppBar position="static">
+      <div class="topbar">
+        <div>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">{props.title}</Typography>
+          </Toolbar>
+        </div>
+        <div>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Search />
+            </IconButton>
+          </Toolbar>
+        </div>
+      </div>
+    </AppBar>
+  );
 }
 
-export default Topbar
+export default Topbar;
