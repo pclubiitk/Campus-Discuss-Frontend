fa6cf24f69c0d000b59334bf825841153776d3f4
Aditya Ranjan <adityaranjan25@gmail.com>
2020-04-11 16:47:38 +0530

    Add minimized post and fix topbar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 
 /*using Material UI*/
 
+import { makeStyles } from "@material-ui/core";
 import { AppBar } from "@material-ui/core";
 import { Toolbar } from "@material-ui/core";
 import { IconButton } from "@material-ui/core";
@@ -10,26 +11,30 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Search from "@material-ui/icons/Search";
 import { Typography } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  topbar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+}));
+
 function Topbar(props) {
+  const classes = useStyles();
+
   return (
-    <AppBar position="static">
-      <div class="topbar">
-        <div>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6">{props.title}</Typography>
-          </Toolbar>
-        </div>
-        <div>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <Search />
-            </IconButton>
-          </Toolbar>
-        </div>
-      </div>
+    <AppBar className={classes.topbar} position="sticky">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">{props.title}</Typography>
+      </Toolbar>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <Search />
+        </IconButton>
+      </Toolbar>
     </AppBar>
   );
 }
