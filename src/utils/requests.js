90c71554cf9bae9c03c48d58d14e1c44adab13be
SD-320808 <sd.320808@gmail.com>
2020-05-15 23:04:26 +0530

    Fixed Issue #36 add request function

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,51 +1,53 @@
-import { useCallback } from "react";
+const base_url = "https://localhost:8000";
 
-const base_url="https://localhost:8000";
-
-const sendRequest = useCallback(
-  async (url, method = "GET", body = null, headers = {}) => {
-    try {
-      const response = await fetch(base_url+ url, {
-        method,
-        body,
-        headers,
-      });
-      const responseData = await response.json();
-      if (!response.ok) throw new Error(responseData.status);
-    } catch (err) {
-      console.log(err);
-    }
-    return { responseData };
+const sendRequest = async (url, method, body, headers = {}) => {
+  try {
+    const response = await fetch(base_url + url, {
+      method,
+      body,
+      headers,
+    });
+    const responseData = await response.json();
+    if (!response.ok) throw new Error(responseData.status);
+  } catch (err) {
+    return err.message;
   }
-);
+  return { responseData };
+};
 
-const login = (username,password) =>{
-    const res= await sendRequest("/users/auth/login/","POST",{username,password});
-    return res;
-}
+const login = async (username, password) => {
+  const res = await sendRequest("/users/auth/login/", "POST", {
+    username,
+    password,
+  });
+  return res;
+};
 
-const getPostsbyStream = () =>{
-    const res=await sendRequest("/streams/<int:pk>/posts/");
-    return res;
-}
+const getPostsbyStream = async (pk) => {
+  const res = await sendRequest("/streams/" + pk + "/posts/", "GET");
+  return res;
+};
 
-const createPost = (title , text , stream) =>{
-    const res= await sendRequest("/posts/create/", "POST" ,{title,text,stream});
-    return res;
+const createPost = async (title, text, stream) => {
+  const res = await sendRequest("/posts/create/", "POST", {
+    title,
+    text,
+    stream,
+  });
+  return res;
+};
 
-}
+const subscribetoStream = async (title) => {
+  const res = await sendRequest("/streams/follow/", "PUT", { title });
+  return res;
+};
 
-const subscribetoStream = (title)=>{
-    const res= await sendRequest("/streams/follow/","PUT",{title});
-    return res;
-}
+const deletePost = async (pk) => {
+  const res = await sendRequest("/posts/delete/", "DELETE", { pk });
+  return res;
+};
 
-const deletePost = (pk)=>{
-    const res= await sendRequest("/posts/delete/","DELETE",{pk});
-    return res;
-}
-
-const fetchPostsbyUser=()=>{
-    const res= await sendRequest("/users/<int:pk>/posts");
-    return res;
-}
+const fetchPostsbyUser = async (pk) => {
+  const res = await sendRequest("/users/" + pk + "/posts", "GET");
+  return res;
+};
